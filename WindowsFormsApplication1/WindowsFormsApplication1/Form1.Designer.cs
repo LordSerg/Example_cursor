b06cb55c7b193de4415a986bd45ec9e5475f0fba
Moskalenko Sergey <oknelaksoms@gmail.com>
2019-03-17 18:48:05 +0200

    17.03.19

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.стеретьВсеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.копироватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.вставитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьКакToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выйтиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(18, 18);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(274, 28);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "Выбрать систему";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.стеретьВсеToolStripMenuItem,
+            this.копироватьToolStripMenuItem,
+            this.вставитьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem,
+            this.сохранитьКакToolStripMenuItem,
+            this.открытьФайлToolStripMenuItem,
+            this.выйтиToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(163, 158);
+            // 
+            // стеретьВсеToolStripMenuItem
+            // 
+            this.стеретьВсеToolStripMenuItem.Name = "стеретьВсеToolStripMenuItem";
+            this.стеретьВсеToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.стеретьВсеToolStripMenuItem.Text = "Стереть все";
+            // 
+            // копироватьToolStripMenuItem
+            // 
+            this.копироватьToolStripMenuItem.Name = "копироватьToolStripMenuItem";
+            this.копироватьToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.копироватьToolStripMenuItem.Text = "Копировать";
+            // 
+            // вставитьToolStripMenuItem
+            // 
+            this.вставитьToolStripMenuItem.Name = "вставитьToolStripMenuItem";
+            this.вставитьToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.вставитьToolStripMenuItem.Text = "Вставить";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            // 
+            // сохранитьКакToolStripMenuItem
+            // 
+            this.сохранитьКакToolStripMenuItem.Name = "сохранитьКакToolStripMenuItem";
+            this.сохранитьКакToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.сохранитьКакToolStripMenuItem.Text = "Сохранить как...";
+            // 
+            // открытьФайлToolStripMenuItem
+            // 
+            this.открытьФайлToolStripMenuItem.Name = "открытьФайлToolStripMenuItem";
+            this.открытьФайлToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.открытьФайлToolStripMenuItem.Text = "Открыть файл...";
+            // 
+            // выйтиToolStripMenuItem
+            // 
+            this.выйтиToolStripMenuItem.Name = "выйтиToolStripMenuItem";
+            this.выйтиToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.выйтиToolStripMenuItem.Text = "Выйти";
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._5;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button5.Location = new System.Drawing.Point(746, 360);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 81);
+            this.button5.TabIndex = 6;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._1;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button4.Location = new System.Drawing.Point(746, 103);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 75);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._3;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.Location = new System.Drawing.Point(746, 275);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 75);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._2;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Location = new System.Drawing.Point(746, 188);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 75);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._4;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(746, 18);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 75);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(586, 679);
+            this.trackBar1.Maximum = 150;
+            this.trackBar1.Minimum = 20;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(236, 45);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Value = 120;
+            this.trackBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 736);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.ControlBox = false;
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem стеретьВсеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem копироватьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem вставитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьКакToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьФайлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выйтиToolStripMenuItem;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Graphics g;
+        int mouse;
+        bool is_selected = false;
+        //1 - default
+        //2 - straight line
+        //3 - draw any line
+        //4 - clear
+        //5 - select
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            base.Text = "";
+            g = CreateGraphics();
+            mouse = 0;
+            comboBox1.Items.Add("p(V)");
+            comboBox1.Items.Add("p(T)");
+            comboBox1.Items.Add("V(T)");
+            //comboBox1.Items.Add("V(ρ)");
+            timer1.Enabled = true;
+            timer1.Interval = 1;
+            this.BackColor = Color.White;
+
+
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mouse = 1;
+            if (is_selected == true)
+            {
+
+            }
+            trackBar1.Visible = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            mouse = 2;
+            if (is_selected == true)
+            {
+
+            }
+            trackBar1.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mouse = 3;
+            if (is_selected == true)
+            {
+
+            }
+            trackBar1.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mouse = 4;
+            trackBar1.Visible = true;
+            if (is_selected==true)
+            {
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            mouse = 5;
+            if (is_selected == true)
+            {
+
+            }
+            trackBar1.Visible = false;
+        }
+        int sx,sy;
+        Point p0,p1;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (mouse == 0)
+            {
+                this.Cursor = new Cursor(GetType(), "3.cur");
+            }
+            //делает разные курсоры при разных действиях  
+            if (mouse == 1)//стандарт
+            {
+                this.Cursor = new Cursor(GetType(), "UO - Gauntlet.cur");
+            }
+            if (mouse == 2)//прямые линии
+            {
+                this.Cursor = new Cursor(GetType(), "UO - move.cur");
+                if (c2 == 1)
+                {                    
+                    g.DrawLine(Pens.White, p0, p1);
+                    p1.X = MousePosition.X - this.Location.X;
+                    p1.Y = MousePosition.Y - this.Location.Y;
+                    g.DrawLine(Pens.Black, p0, p1);
+                }
+            }
+            if (mouse == 3)//кривые лнии
+            {
+                this.Cursor = new Cursor(GetType(), "UO - text.cur");
+            }
+            if (mouse == 4)//стереть
+            {
+                this.Cursor = new Cursor(GetType(), "UO - precision.cur");
+                if(c4==1)
+                {
+                    r1 = trackBar1.Value;
+                    x = MousePosition.X - this.Location.X;
+                    y = MousePosition.Y - this.Location.Y;
+                    g.FillEllipse(Brushes.LightBlue, x - r1/2, y - r1/2, r1, r1);                    
+                    g.FillEllipse(Brushes.White, x - r1 / 2, y - r1 / 2, r1, r1);
+                    sx = x0 - x;
+                    sy = y0 + y;
+                    //for(double ix=0;ix<Math.Abs(sx/10);ix++)
+                    //{
+                    //    //
+                    //}
+                }                
+            }
+            if (mouse == 5)//выделить-передвинуть
+            {
+                
+                this.Cursor = new Cursor(GetType(), "Arrow.cur");
+                if(c5==1)
+                {
+                    x = MousePosition.X - this.Location.X;
+                    y = MousePosition.Y - this.Location.Y;
+                }
+            }
+
+        }
+
+        int r1=120,x,y;
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            is_selected = true;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //записывает нарисованный график в матрицу            
+            //if (mouse == 2)//
+            //{                
+            //    if (c2 == 1)
+            //    {
+            //        g.DrawLine(Pens.White, p0, p1);
+            //        p1.X = MousePosition.X - this.Location.X;
+            //        p1.Y = MousePosition.Y - this.Location.Y;
+            //        g.DrawLine(Pens.Black, p0, p1);
+            //    }
+            //}
+            //if (mouse == 3)
+            //{
+            //    this.Cursor = new Cursor(GetType(), "UO - text.cur");
+            //}
+            //if (mouse == 4)
+            //{
+            //    this.Cursor = new Cursor(GetType(), "UO - precision.cur");
+            //    if (c4 == 1)
+            //    {
+            //        x = MousePosition.X - this.Location.X;
+            //        y = MousePosition.Y - this.Location.Y;
+            //        g.FillEllipse(Brushes.LightBlue, x - r1 / 2, y - r1 / 2, r1, r1);
+            //        g.FillEllipse(Brushes.White, x - r1 / 2, y - r1 / 2, r1, r1);
+
+            //    }
+            //}
+        }
+
+
+
+        int x0, y0, x1, y1, c2 = 0, c3 = 0, c4 = 0, c5 = 0;
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (mouse == 2)
+            {
+                x0 = MousePosition.X - this.Location.X;
+                y0 = MousePosition.Y - this.Location.Y;
+                c2 = 1;
+                p0.X = x0;
+                p0.Y = y0;
+            }
+            if (mouse == 3)
+            {
+                x0 = MousePosition.X - this.Location.X;
+                y0 = MousePosition.Y - this.Location.Y;
+                c3 = 1;
+            }
+            if (mouse == 4)
+            {
+                r1 = trackBar1.Value;
+                c4 = 1;
+                g.DrawEllipse(Pens.White, MousePosition.X - this.Location.X - r1/2, MousePosition.Y - this.Location.Y - r1/2, r1, r1);
+            }
+            if (mouse == 5)
+            {
+                x0 = MousePosition.X - this.Location.X;
+                y0 = MousePosition.Y - this.Location.Y;
+                c5 = 1;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (mouse == 2)
+            {
+                x1 = MousePosition.X - this.Location.X;
+                y1 = MousePosition.Y - this.Location.Y;
+                c2 = 0;
+                g.DrawLine(Pens.Black, x0, y0, x1, y1);
+            }
+            if(mouse==3)
+            {
+                c3 = 0;
+            }
+            if (mouse == 4)
+            {
+                g.FillEllipse(Brushes.White, MousePosition.X - this.Location.X - r1/2, MousePosition.Y - this.Location.Y - r1/2, r1, r1);
+                c4 = 0;
+            }
+            if(mouse==5)
+            {
+                c5 = 0;
+            }
+        }
+    }
+}
